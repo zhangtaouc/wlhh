163509b6e0fbfff986a631e917573f41ac116070
zhangtaouc <39691213+zhangtaouc@users.noreply.github.com>
2023-11-12 17:12:52 +0800

    cui项目完成

--- pages/home/index.js
+++ pages/home/index.js
@@ -20,7 +20,7 @@ Page({
   },
   changeName(event) {
     console.log(event.detail)
-    if (event.detail === 'wlh') {
+    if (event.detail === 'cxl') {
       this.setData({usernameInfo: '正确', isShowFirstVisit: false})
       setTimeout(() => {
         this.setData({usernameInfo: '正确', isShowFirstVisit: false})
@@ -40,51 +40,51 @@ Page({
     if (number === 2) {
       // 万年泉
       let overFlowInfo = { title: '', info: [] }
-      overFlowInfo.title = '万年泉'
-      overFlowInfo.info.push('第二次和你一块吃饭', '那个东西好像火锅，但又不像')
+      overFlowInfo.title = '第一个语音'
+      overFlowInfo.info.push('那晚8点多', '我说我困了', '过一会你就打语音过来', '我瞬间不困了', '很激动')
       this.setData({overFlowInfos: overFlowInfo})
       console.log(overFlowInfo, this.data.overFlowInfos)
     }
     if (number === 3) {
       // 浮山
       let overFlowInfo = { title: '', info: [] }
-      overFlowInfo.title = '浮山'
-      overFlowInfo.info.push('吃完饭我们去了浮山', '那天你穿的美美的', '从那以后便喜欢上你')
+      overFlowInfo.title = '熬夜'
+      overFlowInfo.info.push('那几晚我们聊的很晚', '经常到三四点', '那会你失眠、睡眠不好', '怕你睡不着，等你睡着了我才睡', '第二天虽困但很开心')
       this.setData({overFlowInfos: overFlowInfo})
     }
     if (number === 4) {
       // 丽达
       let overFlowInfo = { title: '', info: [] }
-      overFlowInfo.title = '丽达'
-      overFlowInfo.info.push('这天是6月8号', '第三次见你，去吃了你喜欢的排骨', '从这天开始我们双方陷入思想挣扎', '但我从这一天开始决定“死缠烂打”')
+      overFlowInfo.title = '溜达'
+      overFlowInfo.info.push('不知不觉的', '每天3点左右你都要陪我溜达会', '但是我只能聊15分钟', '每天都期待着那15分钟', '期待着溜达')
       this.setData({overFlowInfos: overFlowInfo})
     }
     if (number === 5) {
       // 院里汤包
       let overFlowInfo = { title: '', info: [] }
-      overFlowInfo.title = '院里汤包'
-      overFlowInfo.info.push('这一天我们去吃了我见过“最可爱的包子”', '由于多加了一笼包子，引发了第二天你吃太饱🤗', '你身穿黑色裙子，真好看')
+      overFlowInfo.title = '打字'
+      overFlowInfo.info.push('不知不觉', '我们开启了另外一种聊天方式🤗', '你说话我打字')
       this.setData({overFlowInfos: overFlowInfo})
     }
     if (number === 6) {
       // 梵高
       let overFlowInfo = { title: '', info: [] }
-      overFlowInfo.title = '我画下了梦'
-      overFlowInfo.info.push('我们去万达看了梵高的画', '梵高的画很漂亮', '也看见了最漂亮的你', '至此，我无法自拔')
+      overFlowInfo.title = '10月的最后一天'
+      overFlowInfo.info.push('我这脑子瓦特了', '给你写了个小作文', '第二天你生病了', '我很惭愧，在你生病的时候我还那样。。。', '很对不起你')
       this.setData({overFlowInfos: overFlowInfo})
     }
     if (number === 7) {
       // 海大
       let overFlowInfo = { title: '', info: [] }
-      overFlowInfo.title = '同学远去'
-      overFlowInfo.info.push('阔别了三年的烧烤摊', '烧烤和烤鱼虽然味道不好', '但和你一块意义非凡', '还好有你')
+      overFlowInfo.title = '11月'
+      overFlowInfo.info.push('11月的开头对你不友好', '生病请假去打针', '期间我又犯毛病了', '惹得你给我写了小作文', '对不起')
       this.setData({overFlowInfos: overFlowInfo})
     }
     if (number === 8) {
       // 丽达
       let overFlowInfo = { title: '', info: [] }
-      overFlowInfo.title = '最后一个夏天'
-      overFlowInfo.info.push('23年最后一个夏天', '你回家两天', '两天没见你，甚是想念', '去丽达吃了炒鸡', '不巧的是车半路坏了0_o')
+      overFlowInfo.title = '王者荣耀'
+      overFlowInfo.info.push('打了三局游戏', '你玩的瑶妹', '地表最强辅助！')
       this.setData({overFlowInfos: overFlowInfo})
     }
     if (number === 9) {
@@ -104,8 +104,8 @@ Page({
     if (number === 100) {
       // 未来
       let overFlowInfo = { title: '', info: [] }
-      overFlowInfo.title = '未来可期'
-      overFlowInfo.info.push('期待和你一起走过更多个夏天', '以后每个夏天都有你')
+      overFlowInfo.title = '未完'
+      overFlowInfo.info.push('待续', '每周一更哦')
       this.setData({overFlowInfos: overFlowInfo})
     }
     this.setData({ isShowMeet: number === 1 });
